293afb18c21797e9a590ee667554b232348d3ae6
Farid Rener <proteusvacuum@gmail.com>
2020-04-06 10:41:10 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="turn-python",
+    version="0.0.1",
+    author="Dimagi, inc",
+    author_email="dev@dimagi.com",
+    description="A python package for the Turn.io WhatsApp API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dimagi/turn-python/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
