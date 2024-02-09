d577fc3093c71b4f4e8f88d6402456ed85458813
Chris Smit <chris.smit@dimagi.com>
2024-02-09 09:10:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="turn-python",
-    version="0.0.2",
+    version="0.1.0",
     author="Dimagi, inc",
     author_email="dev@dimagi.com",
     description="A python package for the Turn.io WhatsApp API",
